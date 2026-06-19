3ae722d3eff18b33998d8a182be7666254b39a2c
Rafael Milani <rafaelgmilani@yahoo.com.br>
2026-06-19 09:20:00 -0300

    config: add Firebase project credentials (crud-will)

--- firebase/config.js
+++ firebase/config.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Credenciais Firebase — substitua pelos valores do seu projeto
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyDT-6l6aYQ2H_wAFVAeQtJgJUdkDbPwrNI",
+  authDomain: "crud-will.firebaseapp.com",
+  projectId: "crud-will",
+  storageBucket: "crud-will.firebasestorage.app",
+  messagingSenderId: "338452438950",
+  appId: "1:338452438950:web:2184c8a4ebc3159dccb967"
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
